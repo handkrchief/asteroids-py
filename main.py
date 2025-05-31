24928773c102e0a3ada72d3ec3510ae8e30ee584
handkrchief <ethancmoore@outlook.com>
2025-05-31 00:24:05 -0700

    added character player sprite, game ticks

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        display.flip()
+        tick = game_clock.tick(60)
+        dt = tick / 1000
+        player.draw(screen)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
